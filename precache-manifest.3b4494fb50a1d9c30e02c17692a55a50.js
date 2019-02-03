6c650b4c723e16f7032bc6572e350a210d3f5579
es2 <es2js2@gmail.com>
2019-02-04 01:35:10 +0900

    Updates

---
+++ precache-manifest.3b4494fb50a1d9c30e02c17692a55a50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f0d25fd81506d48dee",
+    "url": "/movie_app2/static/js/runtime~main.71f0d25f.js"
+  },
+  {
+    "revision": "97f334f4504d0090660e",
+    "url": "/movie_app2/static/js/main.97f334f4.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app2/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "97f334f4504d0090660e",
+    "url": "/movie_app2/static/css/main.72d33d78.chunk.css"
+  },
+  {
+    "revision": "ddcbe775d58b7574f140492a662c2489",
+    "url": "/movie_app2/index.html"
+  }
+];
